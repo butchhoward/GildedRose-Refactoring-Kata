dd945eb3e338bd77de9b69c390d4d318c294d576
Butch Howard <butchhoward@gmail.com>
2021-08-21 15:45:26 -0400

    Add some standard dev setup

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,12 +5,25 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
+    def setUp(self):
+        self.addTypeEqualityFunc(Item, "gilded_rose_item_equal")
+
+    def gilded_rose_item_equal(self, itema, itemb, msg=None):
+        self.assertIsInstance(itema, Item, 'First argument is not an Item')
+        self.assertIsInstance(itemb, Item, 'Second argument is not an Item')
+
+        if itema.name != itemb.name or itema.sell_in != itemb.sell_in or itema.quality != itemb.quality:
+            standard_msg = f"{str(itema)} != {str(itemb)}"
+            self.fail(self._formatMessage(msg, standard_msg))
+
+
+    def test_quality_lower_limit_is_zero_for_normal_products(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
 
-        
+        gilded_rose.update_quality()
+        self.assertEqual(Item("foo", -1, 0), gilded_rose.items[0])
+
+
 if __name__ == '__main__':
     unittest.main()
