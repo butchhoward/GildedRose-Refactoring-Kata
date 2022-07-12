66d8455d0bddc40c83dee620021bb4360808a67e
Butch Howard <butchhoward@gmail.com>
2022-07-12 17:32:52 -0400

    Fill in tests to get code coverage
    start with Normal and Sulfuras

--- cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
+++ cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
@@ -23,3 +23,48 @@ TEST(GildedRoseTest, NormalItemWithSellinGtZeroAndQualityZero)
     EXPECT_EQ(21, app.items[0].sellIn);
     EXPECT_EQ(0, app.items[0].quality);
 }
+
+TEST(GildedRoseTest, NormalItemWithSellinGtZeroAndQualityGtZero)
+{
+    vector<Item> items;
+    items.push_back(Item("Normal", 22, 23));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Normal", app.items[0].name);
+    EXPECT_EQ(21, app.items[0].sellIn);
+    EXPECT_EQ(22, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, NormalItemWithSellinLtZeroAndQualityGtZero)
+{
+    vector<Item> items;
+    items.push_back(Item("Normal", -1, 23));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Normal", app.items[0].name);
+    EXPECT_EQ(-2, app.items[0].sellIn);
+    EXPECT_EQ(21, app.items[0].quality);
+}
+
+
+TEST(GildedRoseTest, SulfurasItemWithSellinGtZeroAndQualityZero)
+{
+    vector<Item> items;
+    items.push_back(Item("Sulfuras, Hand of Ragnaros", 22, 0));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Sulfuras, Hand of Ragnaros", app.items[0].name);
+    EXPECT_EQ(22, app.items[0].sellIn);
+    EXPECT_EQ(0, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, SulfurasItemWithSellinGtZeroAndQualityGtZero)
+{
+    vector<Item> items;
+    items.push_back(Item("Sulfuras, Hand of Ragnaros", 22, 23));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Sulfuras, Hand of Ragnaros", app.items[0].name);
+    EXPECT_EQ(22, app.items[0].sellIn);
+    EXPECT_EQ(23, app.items[0].quality);
+}
