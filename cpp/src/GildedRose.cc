d1f4d035c1c12c0a15a747b04c7e4529f1a3b259
Butch Howard <butchhoward@gmail.com>
2022-07-12 18:35:41 -0400

    string constants in code

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -1,17 +1,21 @@
 #include "GildedRose.h"
 
+static const char* SULFRAS="Sulfuras, Hand of Ragnaros";
+static const char* AGED_BRIE="Aged Brie";
+static const char* TAFKAL80ETC="Backstage passes to a TAFKAL80ETC concert";
+
 GildedRose::GildedRose(vector<Item> & items) : items(items)
 {}
-    
-void GildedRose::updateQuality() 
+
+void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+        if (items[i].name != AGED_BRIE && items[i].name != TAFKAL80ETC)
         {
             if (items[i].quality > 0)
             {
-                if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                if (items[i].name != SULFRAS)
                 {
                     items[i].quality = items[i].quality - 1;
                 }
@@ -23,7 +27,7 @@ void GildedRose::updateQuality()
             {
                 items[i].quality = items[i].quality + 1;
 
-                if (items[i].name == "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name == TAFKAL80ETC)
                 {
                     if (items[i].sellIn < 11)
                     {
@@ -44,20 +48,20 @@ void GildedRose::updateQuality()
             }
         }
 
-        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+        if (items[i].name != SULFRAS)
         {
             items[i].sellIn = items[i].sellIn - 1;
         }
 
         if (items[i].sellIn < 0)
         {
-            if (items[i].name != "Aged Brie")
+            if (items[i].name != AGED_BRIE)
             {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert")
+                if (items[i].name != TAFKAL80ETC)
                 {
                     if (items[i].quality > 0)
                     {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros")
+                        if (items[i].name != SULFRAS)
                         {
                             items[i].quality = items[i].quality - 1;
                         }
