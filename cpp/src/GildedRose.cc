47457562e2e5a19c367b93be034f1d762b5274c6
Butch Howard <butchhoward@gmail.com>
2022-07-12 19:52:39 -0400

    Implement Conjured Items

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -4,6 +4,7 @@ static const char* SULFRAS="Sulfuras, Hand of Ragnaros";
 static const char* AGED_BRIE="Aged Brie";
 static const char* TAFKAL80ETC="Backstage passes to a TAFKAL80ETC concert";
 
+static const char* CONJURED="Conjured";
 
 GildedRose::GildedRose(vector<Item> & items) : items(items)
 {}
@@ -30,6 +31,24 @@ void UpdateNormalItemQuality(Item& item)
         if (item.quality > 0)
         {
             item.quality = item.quality - 1;
+        }
+    }
+}
+
+void UpdateConjuredItemQuality(Item& item)
+{
+    if (item.quality > 0)
+    {
+        item.quality = item.quality - 2;
+    }
+
+    item.sellIn = item.sellIn - 1;
+
+    if (item.sellIn < 0)
+    {
+        if (item.quality > 0)
+        {
+            item.quality = item.quality - 2;
         }
     }
 }
@@ -103,6 +122,11 @@ void GildedRose::updateItemQuality(Item& item)
     if (item.name == TAFKAL80ETC)
     {
         return UpdateTAFKAL80ETCQuality(item);
+    }
+
+    if (item.name == CONJURED)
+    {
+        return UpdateConjuredItemQuality(item);
     }
 
     return UpdateNormalItemQuality(item);
