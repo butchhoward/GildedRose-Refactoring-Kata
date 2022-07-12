2985c624d02bd277b1975d96af058b49b7e53e84
Butch Howard <butchhoward@gmail.com>
2022-07-12 19:23:48 -0400

    remove dead code

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -90,82 +90,20 @@ void UpdateTAFKAL80ETCQuality(Item &item)
 
 void GildedRose::updateItemQuality(Item& item)
 {
-        if (item.name == SULFRAS)
-        {
-            return UpdateSulfurasQuality(item);
-        }
+    if (item.name == SULFRAS)
+    {
+        return UpdateSulfurasQuality(item);
+    }
 
-        if (item.name == AGED_BRIE)
-        {
-            return UpdateAgedBrieQuality(item);
-        }
+    if (item.name == AGED_BRIE)
+    {
+        return UpdateAgedBrieQuality(item);
+    }
 
-        if (item.name == TAFKAL80ETC)
-        {
-            return UpdateTAFKAL80ETCQuality(item);
-        }
+    if (item.name == TAFKAL80ETC)
+    {
+        return UpdateTAFKAL80ETCQuality(item);
+    }
 
-        return UpdateNormalItemQuality(item);
-
-
-        if (item.name != AGED_BRIE && item.name != TAFKAL80ETC)
-        {
-            if (item.quality > 0)
-            {
-                item.quality = item.quality - 1;
-            }
-        }
-        else
-        {
-            if (item.quality < 50)
-            {
-                item.quality = item.quality + 1;
-
-                if (item.name == TAFKAL80ETC)
-                {
-                    if (item.sellIn < 11)
-                    {
-                        if (item.quality < 50)
-                        {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6)
-                    {
-                        if (item.quality < 50)
-                        {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0)
-        {
-            if (item.name != AGED_BRIE)
-            {
-                if (item.name != TAFKAL80ETC)
-                {
-                    if (item.quality > 0)
-                    {
-                        item.quality = item.quality - 1;
-                    }
-                }
-                else
-                {
-                    item.quality = item.quality - item.quality;
-                }
-            }
-            else
-            {
-                if (item.quality < 50)
-                {
-                    item.quality = item.quality + 1;
-                }
-            }
-        }
+    return UpdateNormalItemQuality(item);
 }
