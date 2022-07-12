3f32c4041434f85ae38b1a6603010f494d1ca846
Butch Howard <butchhoward@gmail.com>
2022-07-12 19:20:34 -0400

    Extract Backstage passes

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -39,6 +39,37 @@ void UpdateAgedBrieQuality(Item& item)
 
 }
 
+void UpdateTAFKAL80ETCQuality(Item &item)
+{
+    if (item.quality < 50)
+    {
+        item.quality = item.quality + 1;
+
+        if (item.sellIn < 11)
+        {
+            if (item.quality < 50)
+            {
+                item.quality = item.quality + 1;
+            }
+        }
+
+        if (item.sellIn < 6)
+        {
+            if (item.quality < 50)
+            {
+                item.quality = item.quality + 1;
+            }
+        }
+    }
+
+    item.sellIn = item.sellIn - 1;
+
+    if (item.sellIn < 0)
+    {
+        item.quality = item.quality - item.quality;
+    }
+}
+
 void GildedRose::updateItemQuality(Item& item)
 {
         if (item.name == SULFRAS)
@@ -49,6 +80,11 @@ void GildedRose::updateItemQuality(Item& item)
         if (item.name == AGED_BRIE)
         {
             return UpdateAgedBrieQuality(item);
+        }
+
+        if (item.name == TAFKAL80ETC)
+        {
+            return UpdateTAFKAL80ETCQuality(item);
         }
 
 
