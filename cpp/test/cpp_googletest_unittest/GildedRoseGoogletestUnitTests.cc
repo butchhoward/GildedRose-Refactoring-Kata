5d09be1b52826549803804fd5c9e7f970fac4ad0
Butch Howard <butchhoward@gmail.com>
2022-07-12 14:44:53 -0400

    Test some simple behavior

--- cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
+++ cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
@@ -1,10 +1,25 @@
 #include <gtest/gtest.h>
 #include "GildedRose.h"
 
-TEST(GildedRoseTest, Foo) {
+
+TEST(GildedRoseTest, NormalItemWithZeroSellinAndQuality)
+{
     vector<Item> items;
-    items.push_back(Item("Foo", 0, 0));
+    items.push_back(Item("Normal", 0, 0));
     GildedRose app(items);
     app.updateQuality();
-    EXPECT_EQ("fixme", app.items[0].name);
+    EXPECT_EQ("Normal", app.items[0].name);
+    EXPECT_EQ(-1, app.items[0].sellIn);
+    EXPECT_EQ(0, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, NormalItemWithSellinGtZeroAndQualityZero)
+{
+    vector<Item> items;
+    items.push_back(Item("Normal", 22, 0));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ("Normal", app.items[0].name);
+    EXPECT_EQ(21, app.items[0].sellIn);
+    EXPECT_EQ(0, app.items[0].quality);
 }
