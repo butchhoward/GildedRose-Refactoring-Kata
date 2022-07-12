8b74fc189350184b04a7ebbd710580faa160f66e
Butch Howard <butchhoward@gmail.com>
2022-07-12 18:48:03 -0400

    Extract work out of loop

--- cpp/src/GildedRose.cc
+++ cpp/src/GildedRose.cc
@@ -4,6 +4,7 @@ static const char* SULFRAS="Sulfuras, Hand of Ragnaros";
 static const char* AGED_BRIE="Aged Brie";
 static const char* TAFKAL80ETC="Backstage passes to a TAFKAL80ETC concert";
 
+
 GildedRose::GildedRose(vector<Item> & items) : items(items)
 {}
 
@@ -11,74 +12,79 @@ void GildedRose::updateQuality()
 {
     for (int i = 0; i < items.size(); i++)
     {
-        if (items[i].name != AGED_BRIE && items[i].name != TAFKAL80ETC)
+        updateItemQuality(items[i]);
+    }
+}
+
+void GildedRose::updateItemQuality(Item& item)
+{
+        if (item.name != AGED_BRIE && item.name != TAFKAL80ETC)
         {
-            if (items[i].quality > 0)
+            if (item.quality > 0)
             {
-                if (items[i].name != SULFRAS)
+                if (item.name != SULFRAS)
                 {
-                    items[i].quality = items[i].quality - 1;
+                    item.quality = item.quality - 1;
                 }
             }
         }
         else
         {
-            if (items[i].quality < 50)
+            if (item.quality < 50)
             {
-                items[i].quality = items[i].quality + 1;
+                item.quality = item.quality + 1;
 
-                if (items[i].name == TAFKAL80ETC)
+                if (item.name == TAFKAL80ETC)
                 {
-                    if (items[i].sellIn < 11)
+                    if (item.sellIn < 11)
                     {
-                        if (items[i].quality < 50)
+                        if (item.quality < 50)
                         {
-                            items[i].quality = items[i].quality + 1;
+                            item.quality = item.quality + 1;
                         }
                     }
 
-                    if (items[i].sellIn < 6)
+                    if (item.sellIn < 6)
                     {
-                        if (items[i].quality < 50)
+                        if (item.quality < 50)
                         {
-                            items[i].quality = items[i].quality + 1;
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
             }
         }
 
-        if (items[i].name != SULFRAS)
+        if (item.name != SULFRAS)
         {
-            items[i].sellIn = items[i].sellIn - 1;
+            item.sellIn = item.sellIn - 1;
         }
 
-        if (items[i].sellIn < 0)
+        if (item.sellIn < 0)
         {
-            if (items[i].name != AGED_BRIE)
+            if (item.name != AGED_BRIE)
             {
-                if (items[i].name != TAFKAL80ETC)
+                if (item.name != TAFKAL80ETC)
                 {
-                    if (items[i].quality > 0)
+                    if (item.quality > 0)
                     {
-                        if (items[i].name != SULFRAS)
+                        if (item.name != SULFRAS)
                         {
-                            items[i].quality = items[i].quality - 1;
+                            item.quality = item.quality - 1;
                         }
                     }
                 }
                 else
                 {
-                    items[i].quality = items[i].quality - items[i].quality;
+                    item.quality = item.quality - item.quality;
                 }
             }
             else
             {
-                if (items[i].quality < 50)
+                if (item.quality < 50)
                 {
-                    items[i].quality = items[i].quality + 1;
+                    item.quality = item.quality + 1;
                 }
             }
         }
-    }
 }
