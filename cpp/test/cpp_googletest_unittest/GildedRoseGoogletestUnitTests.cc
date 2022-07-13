bb9c6a2c4a5ad92daba25dd68de7df6096e43eb2
Butch Howard <butchhoward@gmail.com>
2022-07-12 21:08:48 -0400

    Replace GildedRose version of normal tests

--- cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
+++ cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
@@ -4,7 +4,52 @@
 static const char* SULFRAS="Sulfuras, Hand of Ragnaros";
 static const char* AGED_BRIE="Aged Brie";
 static const char* TAFKAL80ETC="Backstage passes to a TAFKAL80ETC concert";
+static const char* NORMAL="Normal";
 
+
+TEST(GildedRoseTest, NormalItemWithZeroSellinAndQuality)
+{
+    vector<Item> items;
+    items.push_back(Item(NORMAL, 0, 0));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ(NORMAL, app.items[0].name);
+    EXPECT_EQ(-1, app.items[0].sellIn);
+    EXPECT_EQ(0, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, NormalItemWithSellinGtZeroAndQualityZero)
+{
+    vector<Item> items;
+    items.push_back(Item(NORMAL, 22, 0));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ(NORMAL, app.items[0].name);
+    EXPECT_EQ(21, app.items[0].sellIn);
+    EXPECT_EQ(0, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, NormalItemWithSellinGtZeroAndQualityGtZero)
+{
+    vector<Item> items;
+    items.push_back(Item(NORMAL, 22, 23));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ(NORMAL, app.items[0].name);
+    EXPECT_EQ(21, app.items[0].sellIn);
+    EXPECT_EQ(22, app.items[0].quality);
+}
+
+TEST(GildedRoseTest, NormalItemWithSellinLtZeroAndQualityGtZero)
+{
+    vector<Item> items;
+    items.push_back(Item(NORMAL, -1, 23));
+    GildedRose app(items);
+    app.updateQuality();
+    EXPECT_EQ(NORMAL, app.items[0].name);
+    EXPECT_EQ(-2, app.items[0].sellIn);
+    EXPECT_EQ(21, app.items[0].quality);
+}
 
 TEST(GildedRoseTest, SulfurasItemWithSellinGtZeroAndQualityZero)
 {
