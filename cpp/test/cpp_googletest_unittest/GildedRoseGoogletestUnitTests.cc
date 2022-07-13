dc659cf0bac8bfb25ac07e57d4679e3133aa46bf
Butch Howard <butchhoward@gmail.com>
2022-07-13 12:54:47 -0400

    Test collection with several items

--- cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
+++ cpp/test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
@@ -6,6 +6,27 @@ static const char* AGED_BRIE="Aged Brie";
 static const char* TAFKAL80ETC="Backstage passes to a TAFKAL80ETC concert";
 static const char* NORMAL="Normal";
 
+TEST(GildedRoseTest, SeveralItemsInCollection)
+{
+    vector<Item> items;
+    items.push_back(Item(NORMAL, 0, 0));
+    items.push_back(Item(SULFRAS, 22, 23));
+    items.push_back(Item(TAFKAL80ETC, 10, 55));
+
+    GildedRose app(items);
+
+    app.updateQuality();
+
+    EXPECT_EQ(0, app.items[0].quality);
+    EXPECT_EQ(-1, app.items[0].sellIn);
+
+    EXPECT_EQ(22, app.items[1].sellIn);
+    EXPECT_EQ(23, app.items[1].quality);
+
+    EXPECT_EQ(9, app.items[2].sellIn);
+    EXPECT_EQ(55, app.items[2].quality);
+
+}
 
 TEST(GildedRoseTest, NormalItemWithZeroSellinAndQuality)
 {
