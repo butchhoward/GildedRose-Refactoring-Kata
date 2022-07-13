ff8a405b8c078458e0a9efe87a4f52493839adb2
Butch Howard <butchhoward@gmail.com>
2022-07-13 15:57:22 -0400

    Separate Brie selling behavior

--- cpp/src/AgedBrieItem.cc
+++ cpp/src/AgedBrieItem.cc
@@ -9,8 +9,6 @@ void AgedBrieItem::updateQuality()
         item.quality = item.quality + 1;
     }
 
-    item.sellIn = item.sellIn - 1;
-
     if (item.sellIn < 0)
     {
         if (item.quality < 50)
@@ -22,5 +20,5 @@ void AgedBrieItem::updateQuality()
 
 void AgedBrieItem::updateSellIn()
 {
-
+    item.sellIn = item.sellIn - 1;
 }
